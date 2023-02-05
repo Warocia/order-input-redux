fb13e1adb49de5b06150189de1bc0d586d4dc9bc
Warocia <rauhala.antti@proton.me>
2023-02-05 20:51:28 +0200

    Navigation done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import OrderUI from './pages/Order/OrderUI';
+import ProductUI from './pages/Product/ProductUI';
 import {Order } from './interfaces/Order';
-
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Sidenav from "./components/Sidenav";
 import './App.css';
 
 
 
 function App() {
   return (
-    <div>
-      <OrderUI/>
+    <div className="MainContainer">
+       <Sidenav/>
+      <div className='MainContainerValue'>
+      <Routes>
+        <Route path="/" element={<OrderUI/>} />
+        <Route path="/productpage" element={<ProductUI/>} />
+      </Routes>
+      </div>
     </div>
   )
 }
---
+++ src/data/navData.tsx
@@ -0,0 +1,17 @@
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import InventoryIcon from '@mui/icons-material/Inventory';
+
+export const navData = [
+    {
+        id: 0,
+        icon: <StorefrontIcon/>,
+        text: "Orders",
+        link: "/"
+    },
+    {
+        id: 1,
+        icon: <InventoryIcon/>,
+        text: "Products",
+        link: "/productpage"
+    }
+]
